bf6472ef613178bf10fbacd2b0b8d85f3d23c4df
PauloAlves8039 <pj38alves@gmail.com>
2021-03-31 15:19:02 -0300

    Atualizando contexto

--- src/SmartSchool.WebAPI/Models/Disciplina.cs
+++ src/SmartSchool.WebAPI/Models/Disciplina.cs
@@ -17,8 +17,8 @@ namespace src.SmartSchool.WebAPI.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public int CargaHoraria { get; set; }
-        public int? PreRequisitoId { get; set; } = null;
-        public Professor PreRequisito { get; set; }
+        public int? PrerequisitoId { get; set; } = null;
+        public Disciplina Prerequisito { get; set; }
         public int ProfessorId { get; set; }
         public Professor Professor { get; set; }
         public int CursoId { get; set; }
