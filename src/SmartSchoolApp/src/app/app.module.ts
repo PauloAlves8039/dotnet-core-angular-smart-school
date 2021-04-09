978390fa85c64d9de916ec2eac26e469284b8ebd
PauloAlves8039 <pj38alves@gmail.com>
2021-04-08 22:38:43 -0300

    Criando projeto SmartSchoolApp para o Front-end

---
+++ src/SmartSchoolApp/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AlunosComponent } from './components/alunos/alunos.component';
+import { ProfessoresComponent } from './components/professores/professores.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TituloComponent } from './components/shared/titulo/titulo.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+
+import { ToastrModule } from 'ngx-toastr';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AlunosComponent,
+    ProfessoresComponent,
+    PerfilComponent,
+    DashboardComponent,
+    NavComponent,
+    TituloComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxSpinnerModule,
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true,
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
